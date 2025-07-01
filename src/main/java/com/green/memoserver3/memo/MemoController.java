be522b0b2376a91a7ae5446fa5fc0b75b8c00881
habi802 <shb8002@gmail.com>
2025-07-01 12:59:34 +0900

    [feat] memo-post에 ip 관련 추가

--- src/main/java/com/green/memoserver3/memo/MemoController.java
+++ src/main/java/com/green/memoserver3/memo/MemoController.java
@@ -3,6 +3,7 @@ package com.green.memoserver3.memo;
 import com.green.memoserver3.config.model.ResultResponse;
 import com.green.memoserver3.memo.model.MemoPostReq;
 import com.green.memoserver3.memo.model.MemoPutReq;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +16,21 @@ public class MemoController {
     private final MemoService memoService;
 
     @PostMapping
-    public ResultResponse<Integer> post(@RequestBody MemoPostReq req) {
+    public ResultResponse<Integer> post(@RequestBody MemoPostReq req, HttpServletRequest request) {
+        req.setIp(getUserIp(request));
         log.info("req={}", req);
         int result = memoService.post(req);
         return new ResultResponse<>("등록 성공", result);
+    }
+
+    public static String getUserIp(HttpServletRequest request) {
+        String ip = request.getRemoteAddr();
+
+        if ("0:0:0:0:0:0:0:1".equals(ip) || "::1".equals(ip)) {
+            ip = "127.0.0.1";
+        }
+
+        return ip;
     }
 
     @PutMapping
--- src/main/java/com/green/memoserver3/memo/model/MemoPostReq.java
+++ src/main/java/com/green/memoserver3/memo/model/MemoPostReq.java
@@ -10,4 +10,5 @@ import lombok.ToString;
 public class MemoPostReq {
     private String title;
     private String content;
+    private String ip;
 }
