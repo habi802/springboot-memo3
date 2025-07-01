0ee0ab888c9564f87b8fd819b3a21e8b314767d1
habi802 <shb8002@gmail.com>
2025-07-01 11:48:10 +0900

    first

---
+++ src/main/java/com/green/memoserver3/MemoServer3Application.java
@@ -0,0 +1,13 @@
+package com.green.memoserver3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoServer3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoServer3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/memoserver3/MemoServer3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.memoserver3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoServer3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
