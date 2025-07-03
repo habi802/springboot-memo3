a40c3bd222f1d1366dec437f53020f4cbf54dec4
habi802 <shb8002@gmail.com>
2025-07-03 14:08:37 +0900

    [feat] get(전체) 검색 부분 옵션 추가

--- src/main/java/com/green/memoserver3/memo/model/MemoGetReq.java
+++ src/main/java/com/green/memoserver3/memo/model/MemoGetReq.java
@@ -8,10 +8,12 @@ import java.beans.ConstructorProperties;
 @Getter
 @ToString
 public class MemoGetReq {
+    String searchOption;
     String searchText;
 
-    @ConstructorProperties({"search_text"})
-    public MemoGetReq(String searchText) {
+    @ConstructorProperties({"search_option", "search_text"})
+    public MemoGetReq(String searchOption, String searchText) {
+        this.searchOption = searchOption;
         this.searchText = searchText;
     }
 }
